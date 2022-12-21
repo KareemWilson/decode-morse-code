eedd2788bbeb7cde3f8176d5ca6926160f01de82
KareemWilson <kareemmagdywilson@gmail.com>
2022-12-21 15:28:42 +0200

    decoding words

---
+++ morse.rb
@@ -0,0 +1,60 @@
+@morse_code = {
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z',
+    '----' => '0',
+    '.----' => '1',
+    '..---' => '2',
+    '...--' => '3',
+    '....-' => '4',
+    '.....' => '5',
+    '-....' => '6',
+    '--...' => '7',
+    '---..' => '8',
+    '----.' => '9'
+}
+
+def decode_char(str)
+    @morse_code[str]
+end
+
+# testing the decode_char func
+puts decode_char('-...')
+
+def decode_word(morse_word)
+    word_arr = morse_word.split
+    word = ''
+    word_arr.each do |char| 
+        word = word + decode_char(char)
+    end
+    word
+end
+
+# testing the decode_word func
+puts decode_word('-- -.--')
+
+
+def decode_morse
